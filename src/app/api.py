07e0c80e63ffb97598c218ccf0f536d5298b6c4a
lancifollia <lancifollia@gmail.com>
2017-02-22 08:22:01 -0500

    (app.server) logging, spv ev

--- src/app/api.py
+++ src/app/api.py
@@ -40,17 +40,20 @@ class RequestHandler(tornado.web.RequestHandler):
 
     @tornado.gen.coroutine
     def post(self, opcode):
-        self.set_header('Content-Type', 'application/json')
-        handler, required_fields = self.post_book.get(opcode, (None, None))
-        data = json.loads(self.request.body)
-        if handler is None or required_fields is None:
-            self.write_error(400, 'Invalid opcode: %s' % opcode)
-            return
-        missing_fields = self.get_missing_fields(data, required_fields)
-        if len(missing_fields) > 0:
-            self.write_error(400, 'Missing fields: %s' % ','.join(list(missing_fields)))
-        else:
-            handler(data)
+        try:
+            self.set_header('Content-Type', 'application/json')
+            handler, required_fields = self.post_book.get(opcode, (None, None))
+            self.logger.info('Query: %s' % self.request.body)
+            data = json.loads(self.request.body)
+            if handler is None or required_fields is None:
+                self.write_error(400, 'Invalid opcode: %s' % opcode)
+            missing_fields = self.get_missing_fields(data, required_fields)
+            if len(missing_fields) > 0:
+                self.write_error(400, 'Missing fields: %s' % ','.join(list(missing_fields)))
+            else:
+                handler(data)
+        except Exception as e:
+            self.write_error(500, 'Internal server error: %s' % str(e))
 
     def is_valid_ts(self, ts):
         if not isinstance(ts, int):
@@ -80,11 +83,11 @@ class RequestHandler(tornado.web.RequestHandler):
     def create_user(self, data):
         try:
             if not self.is_valid_ts(data['birthDay']):
-                self.write({'status': 400, 'msg': 'Invalid birthDay value: %s' % str(data['birthDay'])})
+                self.write_error(400, 'Invalid birthDay value')
             elif not self.is_valid_phone_num(data['phoneNumber']):
-                self.write({'status': 400, 'msg': 'Invalid phone number format: %s (should be 01X-XXXX-XXXX)' % str(data['phoneNumber'])})
+                self.write_error(400, 'Invalid phone number format')
             elif self.is_existing_user(data['phoneNumber']):
-                self.write({'status': 200, 'msg': 'Phone number %s is already exist' % data['phoneNumber'], 'userid': None})
+                self.write_error(400, 'The phone number is already exist')
             else:
                 record = {'userName': data['userName'],
                           'phoneNumber': data['phoneNumber'],
@@ -94,9 +97,9 @@ class RequestHandler(tornado.web.RequestHandler):
                 users = DB.users
                 user_id = users.insert_one(record)
                 self.write({'status': 200, 'msg': 'OK', 'sessionToken': str(user_id.inserted_id)})
+            self.finish()
         except Exception as e:
-            self.write({'status': 500, 'msg': str(e)})
-        self.finish()
+            self.write_error(500, str(e))
 
     @tornado.gen.coroutine
     def get_user_info(self, data):
@@ -106,10 +109,10 @@ class RequestHandler(tornado.web.RequestHandler):
                 record['_id'] = str(record['_id'])
                 self.write({'status': 200, 'msg': 'OK', 'user': record})
             else:
-                self.write({'status': 200, 'msg': 'Not exist', 'user': None})
+                self.write({'status': 400, 'msg': 'Not exist', 'user': None})
+                self.finish()
         except Exception as e:
-            self.write({'status': 500, 'msg': str(e)})
-        self.finish()
+            self.write_error(500, str(e))
 
     @tornado.gen.coroutine
     def add_willitem(self, data):
@@ -156,11 +159,12 @@ class RequestHandler(tornado.web.RequestHandler):
 if __name__ == "__main__":
     global SVR
     global DB
+    global LOGGER
 
     port = sys.argv[1]
     logging.basicConfig(format='%(asctime)-15s %(message)s')
-    logger = logging.getLogger('app_server')
-    opts = dict(logger=logger)
+    LOGGER = logging.getLogger('app_server')
+    opts = dict(logger=LOGGER)
 
     client = pymongo.MongoClient()
     DB = client.test_database
@@ -168,7 +172,7 @@ if __name__ == "__main__":
 
     application = tornado.web.Application([(r'/app/(.+)', RequestHandler, opts)])
 
-    logger.info('Ready to serve. (port: %s)' % port)
+    LOGGER.info('Ready to serve. (port: %s)' % port)
 
     SVR = tornado.httpserver.HTTPServer(application)
     SVR.listen(int(port))
